223ebb1625c1a17ebe82831614819904eb9869f8
irfan nurzaman <inurzmn@gmail.com>
2024-01-26 02:46:53 +0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,56 +4,65 @@ import vitePluginImp from 'vite-plugin-imp';
 
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-  
-//   css: {
-//     preprocessorOptions: {
-      // less: {
-      //   math: "always",
-      //   relativeUrls: true,
-      //   javascriptEnabled: true,
-      // },
-//     },
-//   },
-// })
-
 export default defineConfig({
-  // base: '/vite-react/',
-  plugins: [
-    react(),
-    vitePluginImp({
-      libList: [
-        {
-          libName: 'antd',
-          style: (name) => `antd/es/${name}/style`,
-        },
-      ],
-    }),
-  ],
-  server: {
-    // open: true,
-    port: 3000,
-  },
-  resolve: {
-    alias: [
-      // { find: '@', replacement: path.resolve(__dirname, 'src') },
-      // fix less import by: @import ~
-      // https://github.com/vitejs/vite/issues/2185#issuecomment-784637827
-      { find: /^~/, replacement: '' },
-    ],
-  },
+  plugins: [react()],
+  
   css: {
-    modules: {
-      localsConvention: 'camelCaseOnly',
-    },
     preprocessorOptions: {
       less: {
         math: "always",
         relativeUrls: true,
         javascriptEnabled: true,
-        additionalData: '@root-entry-name: default;',
       },
     },
   },
 })
+
+// export default defineConfig({
+//   // base: '/vite-react/',
+//   plugins: [
+//     react(),
+//     vitePluginImp({
+//       libList: [
+//         {
+//           libName: 'antd',
+//           style: (name) => `antd/es/${name}/style`,
+//         },
+//       ],
+//     }),
+//   ],
+//   server: {
+//     // open: true,
+//     port: 3000,
+//   },
+//   resolve: {
+//     alias: [
+//       // { find: '@', replacement: path.resolve(__dirname, 'src') },
+//       // fix less import by: @import ~
+//       // https://github.com/vitejs/vite/issues/2185#issuecomment-784637827
+//       { find: /^~/, replacement: '' },
+//     ],
+//   },
+//   css: {
+//     modules: {
+//       localsConvention: 'camelCaseOnly',
+//     },
+//     preprocessorOptions: {
+//       less: {
+//         math: "always",
+//         relativeUrls: true,
+//         javascriptEnabled: true,
+//         additionalData: '@root-entry-name: default;',
+//       },
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           'react-venders': ['react', 'react-dom', '@vitjs/runtime'],
+//         },
+//       },
+//     },
+//   },
+// })
